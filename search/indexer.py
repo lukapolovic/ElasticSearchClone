2a9307677477d1ebcdff3854075e83fa8867a79d
polovni <luka.polovic11@gmail.com>
2025-12-11 21:49:49 +0100

    improvements to the indexer and query engine, weighted fields implemented

--- search/indexer.py
+++ search/indexer.py
@@ -5,26 +5,32 @@ class Indexer:
     def __init__(self):
         self.index = {}
 
-    def add_document(self, doc_id, text):
+    def add_document(self, doc_id, field_name, text):
         tokens = tokenize(text)
 
         for token in tokens:
             if token not in self.index:
-                self.index[token] = set()
+                self.index[token] = {}
             
-            self.index[token].add(doc_id)
+            if doc_id not in self.index[token]:
+                self.index[token][doc_id] = set()
+            
+            self.index[token][doc_id].add(field_name)
 
     def build(self, documents, fields):
         for doc in documents:
             doc_id = doc["id"]
 
-            parts = []
             for field in fields:
-                parts.append(doc[field])
-            
-            combined_text = " ".join(parts)
+                field_text = doc[field]
 
-            self.add_document(doc_id, combined_text)
+                if isinstance(field_text, list):
+                    field_text = " ".join(field_text)
+                else:
+                    field_text = str(field_text)
+                    
+                self.add_document(doc_id, field, field_text)
 
     def lookup(self, token):
-        return self.index.get(token, set()).copy()
+        postings = self.index.get(token, {})
+        return {doc_id: fields.copy() for doc_id, fields in postings.items()}
--- search/query.py
+++ search/query.py
@@ -1,28 +1,42 @@
 from search.tokenizer import tokenize
 
+FIELD_WEIGHTS = {
+    "title": 5.0,
+    "cast": 4.0,
+    "director": 3.0,
+    "genres": 3.0,
+    "description": 1.0,
+    "year": 0.5,
+    "rating": 0.1
+}
+
+
 class QueryEngine:
     def __init__(self, indexer):
         self.indexer = indexer
 
     def search(self, query_string):
         if not query_string:
-            return set()
+            return []
         
         tokens = tokenize(query_string)
         if not tokens:
-            return set()
+            return []
+        
+        scores = {}
 
-        posting_lists = []
         for token in tokens:
-            docs = self.indexer.lookup(token)
-            if not docs:
-                return set()
-            posting_lists.append(docs)
+            matches = self.indexer.lookup(token)
 
-        result = posting_lists[0].copy()
+            for doc_id, fields in matches.items():
+                if doc_id not in scores:
+                    scores[doc_id] = 0.0
 
-        for docs in posting_lists[1:]:
-            result &= docs
+                for field in fields:
+                    weight = FIELD_WEIGHTS.get(field, 0.0)
+                    scores[doc_id] += weight
+            
+            ranked_docs = sorted(scores.items(), key=lambda x: x[1], reverse=True)
 
-        return result
+            return [doc_id for doc_id, _ in ranked_docs]
             
