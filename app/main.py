d5c8f1e1117a79e6ccd8b3a3617d7821ebcd5182
polovni <luka.polovic11@gmail.com>
2025-12-30 21:19:32 +0100

    single node arhitecture search service state bug fix

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-import os
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
@@ -8,22 +7,19 @@ from app.api.errors import search_error_handler
 from app.core.exceptions import SearchError
 from search.nltk_setup import ensure_nltk_data
 
-search_service = SearchService()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    ensure_nltk_data() 
+    ensure_nltk_data()
     app.state.search_service = SearchService()
     app.state.search_service.load_data()
     yield
 
+
 app = FastAPI(
     title="ElasticSearch Clone",
-    lifespan=lifespan
+    lifespan=lifespan,
 )
 
 app.include_router(search.router, prefix="/search", tags=["search"])
-
 app.add_exception_handler(SearchError, search_error_handler)
-
-app.state.search_service = search_service
